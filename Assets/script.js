3fc7e1323b0b6c770a7612526584b7acead6497f
George <gms1980@icloud.com>
2021-02-19 18:41:29 -0700

    modified code

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,9 @@
+
 // select all elements
-const start = document.getElementById("Start");
+const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
+//const qImg = document.getElementById("qImg");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
@@ -12,39 +13,34 @@ const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
-
-
 // create our questions
 let questions = [
     {
-        question : "Commonly used data types do not inlude?",
-        imgSrc : "img/html.png",
-        choiceA : "string",
-        choiceB : "booleans",
-        choiceC : "alerts",
-        choiceD : "numbers",
+        question : "What does HTML stand for?",
+        //imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        choiceD : "wrongRight answer",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        //imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        choiceD : "Wrong",
         correct : "B"
     },{
-        question : "The condition in an if/else statement is enclosed within ____?",
-        imgSrc : "img/css.png",
-        choiceA : "quotes",
-        choiceB : "curly brackets",
-        choiceC : "parenthesis",
-        choiceD : "square brackets",
-        correct : "B"
-    },{
-        question : "Arrays in JavaScript can be stored to use?",
-        imgSrc : "img/js.png",
-        choiceA : "numbers and strings",
-        choiceB : "terminal bash",
-        choiceC : "Booleans",
-        choiceD : "All of the Above",
+        question : "What does JS stand for?",
+        //imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        choiceD : "Wrong",
         correct : "C"
     }
 ];
-
-start.addEventListener("click",startQuiz);
-console.log(startQuiz)
 
 // create some variables
 
@@ -62,13 +58,14 @@ function renderQuestion(){
     let q = questions[runningQuestion];
     
     question.innerHTML = "<p>"+ q.question +"</p>";
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+   // qImg.innerHTML = "<img src="+ q.imgSrc +">";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
     choiceD.innerHTML = q.choiceD;
 }
 
+start.addEventListener("click",startQuiz);
 
 // start quiz
 function startQuiz(){
@@ -80,8 +77,6 @@ function startQuiz(){
     TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
 }
 
-
-
 // render progress
 function renderProgress(){
     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
@@ -92,73 +87,88 @@ function renderProgress(){
 // counter render
 
 function renderCounter(){
-  if(count <= questionTime){
-      counter.innerHTML = count;
-      timeGauge.style.width = count * gaugeUnit + "px";
-      count++
-  }else{
-      count = 0;
-      // change progress color to red
-      answerIsWrong();
-      if(runningQuestion < lastQuestion){
-          runningQuestion++;
-          renderQuestion();
-      }else{
-          // end the quiz and show the score
-          clearInterval(TIMER);
-          scoreRender();
-      }
-  }
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
 }
 
 // checkAnwer
 
 function checkAnswer(answer){
-  if( answer == questions[runningQuestion].correct){
-      // answer is correct
-      score++;
-      // change progress color to green
-      answerIsCorrect();
-  }else{
-      // answer is wrong
-      // change progress color to red
-      answerIsWrong();
-  }
-  count = 0;
-  if(runningQuestion < lastQuestion){
-      runningQuestion++;
-      renderQuestion();
-  }else{
-      // end the quiz and show the score
-      clearInterval(TIMER);
-      scoreRender();
-  }
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
 }
 
 // answer is correct
 function answerIsCorrect(){
-  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
 }
 
 // answer is Wrong
 function answerIsWrong(){
-  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
 }
 
 // score render
 function scoreRender(){
-  scoreDiv.style.display = "block";
-  
-  // calculate the amount of question percent answered by the user
-  const scorePerCent = Math.round(100 * score/questions.length);
-  
-  // choose the image based on the scorePerCent
-  let img = (scorePerCent >= 80) ? "img/5.png" :
-            (scorePerCent >= 60) ? "img/4.png" :
-            (scorePerCent >= 40) ? "img/3.png" :
-            (scorePerCent >= 20) ? "img/2.png" :
-            "img/1.png";
-  
-  scoreDiv.innerHTML = "<img src="+ img +">";
-  scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    // let img = (scorePerCent >= 80) ? "img/5.png" :
+    //           (scorePerCent >= 60) ? "img/4.png" :
+    //           (scorePerCent >= 40) ? "img/3.png" :
+    //           (scorePerCent >= 20) ? "img/2.png" :
+    //           "img/1.png";
+    
+    //scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
